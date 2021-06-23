30f041e075c0927a73ddfa9f8d92e1f37f39d195
jeanniffer <jeannifferp@gmail.com>
2021-06-23 15:21:56 -0400

    Modificaciones sobre arca

--- pages/sobre-arca/index.vue
+++ pages/sobre-arca/index.vue
@@ -9,22 +9,9 @@
     </template>
 
     <template v-else>
-      <div class="sobre-arca">
-        <h1>{{ pagina.titulo }}</h1>
-        <p>{{ pagina.descripcion }}</p>
-        <div class="descripcion">
-          <p>
-            "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti
-            atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique
-            sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum
-            facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil
-            impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor
-            repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et
-            voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus,
-            ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."
-          </p>
-        </div>
-      </div>
+      <h1>{{ pagina.titulo }}</h1>
+      <p>{{ pagina.descripcion }}</p>
+      <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti</p>
     </template>
   </div>
 </template>
