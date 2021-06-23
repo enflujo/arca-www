2cd0661c1316fa2cb687ba419bc0b174896ec7dd
Antonia Bustamante <anattolia@gmail.com>
2021-06-22 21:58:43 -0500

    :octocat: creación de página sobre arca y ejemplo mínimo css

--- pages/index.vue
+++ pages/index.vue
@@ -87,6 +87,7 @@ section {
   min-height: 50vh;
   position: relative;
   z-index: 1;
+  padding: 0.8em;
 }
 
 #portada {
---
+++ pages/sobre-arca/index.vue
@@ -0,0 +1,97 @@
+<template>
+  <div>
+    <template v-if="$fetchState.pending">
+      <h1>Pendiente...</h1>
+    </template>
+
+    <template v-else-if="$fetchState.error">
+      <h1 class="error">{{ $fetchState.error.message }}</h1>
+    </template>
+
+    <template v-else>
+      <div class="sobre-arca">
+        <h1>{{ pagina.titulo }}</h1>
+        <p>{{ pagina.descripcion }}</p>
+        <div class="descripcion">
+          <p>
+            "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti
+            atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique
+            sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum
+            facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil
+            impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor
+            repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et
+            voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus,
+            ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."
+          </p>
+        </div>
+      </div>
+    </template>
+  </div>
+</template>
+
+<script>
+import { gql } from 'nuxt-graphql-request';
+import { crearHead } from '../../utilidades/ayudas';
+
+export default {
+  data() {
+    return {
+      pagina: {},
+      obras: [],
+    };
+  },
+
+  async fetch() {
+    const query = gql`
+      query {
+        paginas(filter: { slug: { _eq: "sobre-arca" } }, limit: 1) {
+          titulo
+          slug
+          descripcion
+          contenido
+          banner {
+            id
+            title
+          }
+        }
+      }
+    `;
+
+    const { paginas, artworks } = await this.$graphql.principal.request(query);
+
+    if (paginas.length && paginas[0].slug) {
+      this.pagina = paginas[0];
+    } else {
+      if (process.server) {
+        this.$nuxt.context.res.statusCode = 404;
+      }
+      throw new Error('La página no existe');
+    }
+
+    if (artworks && artworks.length) {
+      this.obras = artworks;
+    }
+  },
+
+  head() {
+    return crearHead(
+      this.$store.state.general.datos.nombre,
+      this.pagina.titulo,
+      this.pagina.descripcion,
+      this.pagina.banner,
+      this.$nuxt.$route.path
+    );
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.sobre-arca {
+  margin: 3em;
+  max-width: 90%;
+}
+
+.descripcion {
+  padding-top: 1em;
+}
+</style>
