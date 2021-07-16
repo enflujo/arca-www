936716b07f4652bbe952ee14b74866c8dc0671bb
Juan Camilo González Jiménez <info@juancgonzalez.com>
2021-07-16 18:23:27 -0500

    ⚡️ optimizar el buscador: guarda resultados

--- store/buscador.js
+++ store/buscador.js
@@ -2,12 +2,21 @@ import { gql } from 'nuxt-graphql-request';
 // simil a data
 export const state = () => ({
   seleccionados: [],
+  guardados: {},
 });
 
 export const actions = {
   //
-  async buscar({ commit }, filtro) {
-    const query = gql`
+  async buscar({ commit, state }, filtro) {
+    /**
+     * si existe el campo en guardados && si existe lo que se esta buscando de ese campo,
+     * usa los elementos guardados en `state.guardados`.
+     * De lo contrario, hace la petición al servidor.
+     */
+    if (state.guardados[filtro.campo] && state.guardados[filtro.campo][filtro.comparacion]) {
+      commit('usarGuardados', state.guardados[filtro.campo][filtro.comparacion]);
+    } else {
+      const query = gql`
       query {
         artworks(filter: { ${filtro.campo}: { id: { _eq: ${filtro.comparacion} } } } limit: -1) {
           id
@@ -29,13 +38,28 @@ export const actions = {
       }
     `;
 
-    const { artworks } = await this.$graphql.principal.request(query);
-    commit('guardarSeleccionados', artworks);
+      const { artworks } = await this.$graphql.principal.request(query);
+      commit('guardarSeleccionados', {
+        artworks,
+        filtro,
+      });
+    }
   },
 };
 
 export const mutations = {
-  guardarSeleccionados(state, seleccionados) {
-    state.seleccionados = seleccionados;
+  guardarSeleccionados(state, datos) {
+    const guardados = state.guardados;
+    if (!guardados[datos.filtro.campo]) {
+      guardados[datos.filtro.campo] = {};
+    }
+
+    guardados[datos.filtro.campo][datos.filtro.comparacion] = datos.artworks;
+
+    state.guardados = guardados;
+    state.seleccionados = datos.artworks;
+  },
+  usarGuardados(state, artworks) {
+    state.seleccionados = artworks;
   },
 };
