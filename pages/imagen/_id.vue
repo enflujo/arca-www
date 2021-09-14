529bfde475ae479e5340dfe5b0adc08c79fbfc7e
Antonia Bustamante <anattolia@gmail.com>
2021-09-13 20:05:59 -0500

    arreglitos página imagen

--- pages/imagen/_id.vue
+++ pages/imagen/_id.vue
@@ -9,74 +9,73 @@
     </template>
 
     <template v-else>
-      <div class="completo-archivo">
-        <h1>{{ obra.title }}</h1>
-        <h3 class="nombre-autor">{{ `${obra.author_id.name} ${obra.author_id.lastname}` }}</h3>
-        <div class="descripcion">
-          <img :src="urlImagen(obra.image)" :alt="obra.title" />
-          <!-- <p>{{ obra.annotation_date }}</p> -->
+      <div class="contenedor-centrado">
+        <div class="completo-archivo">
+          <div class="titulo">
+            <h1>{{ obra.title }}</h1>
+            <h3 class="nombre-autor">{{ `${obra.author_id.name} ${obra.author_id.lastname}` }}</h3>
+          </div>
+          <div class="descripcion">
+            <img :src="urlImagen(obra.image)" :alt="obra.title" />
+            <!-- <p>{{ obra.annotation_date }}</p> -->
+          </div>
+          <div class="botonera">
+            <button class="botones-imagen">datos</button>
+            <button class="botones-imagen">descripcion</button>
+            <button class="botones-imagen">personajes y gestos</button>
+            <button class="botones-imagen">proyectos</button>
+          </div>
+          <section class="informacion-general">
+            <!-- Los títulos deberian venir de las bases de datos tambien. -->
+            <div class="linea">
+              <div class="titulo">Título</div>
+              <div class="descripcion">{{ obra.title }}</div>
+            </div>
+            <div class="linea">
+              <div class="titulo">Autor</div>
+              <div class="descripcion">{{ obra.author_id.name }} {{ obra.author_id.lastname }}</div>
+            </div>
+            <div class="linea">
+              <div class="titulo">titulo ejemplo</div>
+              <div class="descripcion">ejemplo de descripcion lorem ipsum</div>
+            </div>
+            <div class="linea">
+              <div class="titulo">titulo ejemplo</div>
+              <div class="descripcion">ejemplo de descripcion lorem ipsum</div>
+            </div>
+            <div class="linea">
+              <div class="titulo">titulo ejemplo</div>
+              <div class="descripcion">ejemplo de descripcion lorem ipsum</div>
+            </div>
+            <div class="linea">
+              <div class="titulo">titulo ejemplo</div>
+              <div class="descripcion">ejemplo de descripcion lorem ipsum</div>
+            </div>
+            <div class="linea">
+              <div class="titulo">titulo ejemplo</div>
+              <div class="descripcion">ejemplo de descripcion lorem ipsum</div>
+            </div>
+            <div class="linea">
+              <div class="titulo">titulo ejemplo</div>
+              <div class="descripcion">ejemplo de descripcion lorem ipsum</div>
+            </div>
+          </section>
+          <section class="informacion-geografica">
+            <h3 class="titulo">Ubicación</h3>
+            <div class="galeria-mapas">
+              <div class="mapa">
+                <h5 class="subtitulo">País de origen</h5>
+                <div class="ubicacion"></div>
+                <h5 class="pais">{{ obra.actual_country_id.name_spanish }}</h5>
+              </div>
+              <div class="mapa">
+                <h5 class="subtitulo">País actual</h5>
+                <div class="ubicacion"></div>
+                <h5 class="pais">{{ obra.actual_country_id.name_spanish }}</h5>
+              </div>
+            </div>
+          </section>
         </div>
-        <div class="botonera">
-          <button class="botones-imagen">datos</button>
-          <button class="botones-imagen">descripcion</button>
-          <button class="botones-imagen">personajes y gestos</button>
-          <button class="botones-imagen">proyectos</button>
-        </div>
-        <section class="informacion-general">
-          <!-- Los títulos deberian venir de las bases de datos tambien. -->
-          <div class="linea">
-            <div class="titulo">Título</div>
-            <div class="descripcion">{{ obra.title }}</div>
-          </div>
-          <div class="linea">
-            <div class="titulo">Autor</div>
-            <div class="descripcion">{{ obra.author_id.name }} {{ obra.author_id.lastname }}</div>
-          </div>
-          <div class="linea">
-            <div class="titulo">titulo ejemplo</div>
-            <div class="descripcion">ejemplo de descripcion lorem ipsum</div>
-          </div>
-          <div class="linea">
-            <div class="titulo">titulo ejemplo</div>
-            <div class="descripcion">ejemplo de descripcion lorem ipsum</div>
-          </div>
-          <div class="linea">
-            <div class="titulo">titulo ejemplo</div>
-            <div class="descripcion">ejemplo de descripcion lorem ipsum</div>
-          </div>
-          <div class="linea">
-            <div class="titulo">titulo ejemplo</div>
-            <div class="descripcion">ejemplo de descripcion lorem ipsum</div>
-          </div>
-          <div class="linea">
-            <div class="titulo">titulo ejemplo</div>
-            <div class="descripcion">ejemplo de descripcion lorem ipsum</div>
-          </div>
-          <div class="linea">
-            <div class="titulo">titulo ejemplo</div>
-            <div class="descripcion">ejemplo de descripcion lorem ipsum</div>
-          </div>
-        </section>
-        <section class="informacion-geografica">
-          <h3 class="titulo">ubicacion</h3>
-          <div class="galeria-mapas">
-            <div class="mapa">
-              <h5 class="subtitulo">pais de origen</h5>
-              <div class="ubicacion"></div>
-              <h5 class="pais">mexico</h5>
-            </div>
-            <div class="mapa">
-              <h5 class="subtitulo">ciudad de origen</h5>
-              <div class="ubicacion"></div>
-              <h5 class="pais">mexico</h5>
-            </div>
-            <div class="mapa">
-              <h5 class="subtitulo">pais de pais actual</h5>
-              <div class="ubicacion"></div>
-              <h5 class="pais">mexico</h5>
-            </div>
-          </div>
-        </section>
       </div>
     </template>
   </div>
@@ -111,6 +110,10 @@ export default {
             id
             name
             lastname
+          }
+          actual_country_id {
+            id
+            name_spanish
           }
         }
       }
@@ -147,18 +150,25 @@ export default {
 </script>
 <style lang="scss" scoped>
 main {
-  // flex-grow: initial;
+  justify-content: center;
 }
 
 .contenedor-centrado {
   display: flex;
   width: 100vw;
   justify-content: center;
+  overflow: hidden;
+
   .completo-archivo {
     padding-top: 100px;
     overflow: hidden;
     width: 1100px;
     padding-bottom: 100px;
+  }
+  .titulo {
+    justify-content: center;
+    display: grid;
+    text-align: center;
   }
 }
 
@@ -167,6 +177,7 @@ h5 {
   font-weight: 300;
   letter-spacing: 4px;
   text-transform: uppercase;
+  padding-right: 4em;
 }
 .nombre-autor {
   font-family: $fuenteMenu;
@@ -246,6 +257,7 @@ img {
       display: flex;
       align-items: center;
       flex-direction: column;
+      margin-top: 1em;
       .subtitulo {
       }
       .ubicacion {
