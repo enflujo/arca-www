ec4e28772e413bef570de40a8fbd21283569824b
Antonia Bustamante <anattolia@gmail.com>
2021-09-19 19:07:11 -0500

    :dango: agregar estilo de archivo a página autor

--- pages/archivo/_autor.vue
+++ pages/archivo/_autor.vue
@@ -22,17 +22,83 @@
             <div class="todas-images">
               <div v-for="(obra, i) in obras" :key="`obra-${i}`" class="imagen">
                 <nuxt-link :to="`/imagen/${obra.id}`"
-                  ><img :src="urlImagen(obras[i].image)" :alt="obras.title"
-                /></nuxt-link>
+                  ><img :src="urlImagen(obras[i].image)" :alt="obras.title" />
+                  <div class="informacion-hover">
+                    <h2 class="nombre-hover">{{ obra.title }}</h2>
+                    <h3 class="nombre-hover">{{ obra.author_id.name }} {{ obra.author_id.lastname }}</h3>
+                    <p class="descripcion-hover">{{ obra.synthesis }}</p>
+                  </div>
+                </nuxt-link>
               </div>
             </div>
             <div class="barra-detalles">
-              <div class="descripcion-categoria">
-                <h3>{{ busquedaActual }}</h3>
-                <!-- TODO: Hay un bug 
+              <div class="cambio-vista">
+                <svg xmlns="http://www.w3.org/2000/svg" width="29" height="29" viewBox="0 0 29 29">
+                  <g id="Group_595" data-name="Group 595" transform="translate(-1853 -447)">
+                    <g id="Group_585" data-name="Group 585" transform="translate(1853.472 447)">
+                      <path
+                        id="Ellipse_57"
+                        data-name="Ellipse 57"
+                        d="M14.5,2A12.5,12.5,0,0,0,5.661,23.339,12.5,12.5,0,0,0,23.339,5.661,12.418,12.418,0,0,0,14.5,2m0-2A14.5,14.5,0,1,1,0,14.5,14.5,14.5,0,0,1,14.5,0Z"
+                        transform="translate(-0.472 0)"
+                        fill="#af2828"
+                      />
+                    </g>
+                    <g id="Group_586" data-name="Group 586" transform="translate(20 10)">
+                      <rect
+                        id="Rectangle_139"
+                        data-name="Rectangle 139"
+                        width="5"
+                        height="5"
+                        transform="translate(1842 446)"
+                        fill="#af2828"
+                      />
+                      <rect
+                        id="Rectangle_141"
+                        data-name="Rectangle 141"
+                        width="5"
+                        height="5"
+                        transform="translate(1842 452)"
+                        fill="#af2828"
+                      />
+                      <rect
+                        id="Rectangle_140"
+                        data-name="Rectangle 140"
+                        width="5"
+                        height="5"
+                        transform="translate(1848 446)"
+                        fill="#af2828"
+                      />
+                      <rect
+                        id="Rectangle_142"
+                        data-name="Rectangle 142"
+                        width="5"
+                        height="5"
+                        transform="translate(1848 452)"
+                        fill="#af2828"
+                      />
+                    </g>
+                  </g>
+                </svg>
+                <input type="checkbox" id="switch" /><label for="switch">Toggle</label>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24.393" height="24.403" viewBox="0 0 24.393 24.403">
+                  <g id="Group_581" data-name="Group 581" transform="translate(0 0)">
+                    <path
+                      id="Path_1123"
+                      data-name="Path 1123"
+                      d="M79.785,134.263A12.216,12.216,0,0,0,90.793,116.8c-.012-.033-.026-.065-.041-.1s-.025-.045-.038-.067a12.2,12.2,0,1,0-10.93,17.63ZM88.191,116.7,80.9,120.254v-8.124a9.9,9.9,0,0,1,7.287,4.573Zm-9.525-4.572v9.915a1.12,1.12,0,0,0,1.61,1.006l8.893-4.335a9.952,9.952,0,1,1-10.5-6.586Z"
+                      transform="translate(-67.587 -109.859)"
+                      fill="#c6c6c6"
+                    />
+                  </g>
+                </svg>
+              </div>
+              <!--<div class="descripcion-categoria"> -->
+              <h3>{{ busquedaActual }}</h3>
+              <!-- TODO: Hay un bug 
                 <p class="descripcion">{{ obrasSeleccionadas[0].author_id.biography }}</p>
                 -->
-              </div>
+              <!-- </div> -->
               <Mapa />
             </div>
           </div>
@@ -153,27 +219,57 @@ export default {
 .barra-detalles {
   display: flex;
   flex-direction: column;
+  width: 5%;
   justify-content: space-around;
+  align-content: center;
 }
 .agrupar-elementos {
   display: flex;
 }
 .todas-images {
   display: grid;
-  width: 70%;
+  width: 96%;
   height: calc(100vh - 40px);
-  grid-template-columns: repeat(3, 1fr);
   grid-gap: 10px;
   padding: 10px;
-  grid-auto-rows: minmax(100px, auto);
+  grid-template-columns: repeat(auto-fill, minmax(180px, 1fr));
+  grid-auto-rows: minmax(180px, auto);
+  grid-auto-flow: dense;
   .imagen {
+    border-radius: 10px;
+    overflow: hidden;
+  }
+  .informacion-hover {
+    color: $claridad !important;
+    padding: 20px;
+    opacity: 0;
     background-color: $dolor;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    text-transform: none;
+    z-index: 999999;
+    .nombre-hover {
+      color: $claridad;
+      letter-spacing: 0;
+    }
+    .descripcion-hover {
+      letter-spacing: 0;
+      margin-top: 10px;
+      line-height: 1.5;
+    }
+  }
+  .informacion-hover:hover {
+    opacity: 0.8;
+    transition: 0.5s;
   }
 }
+
 .descripcion-categoria {
   background-color: $dolor;
   width: 350px;
-  height: 250px;
+  height: 200px;
   color: $claridad;
   padding: 10px;
   position: relative;
@@ -197,7 +293,60 @@ li {
 }
 .descripcion {
   margin-top: 10px;
-  font-size: 0.8em;
+}
+
+input[type='checkbox'] {
+  height: 0;
+  width: 0;
+  visibility: hidden;
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+
+label {
+  cursor: pointer;
+  text-indent: -9999px;
+  width: 50px;
+  height: 30px;
+  border: 2px solid $dolor;
+  background: transparent !important;
+  display: block;
+  border-radius: 100px;
+  position: relative;
+  transform: rotate(90deg);
+}
+
+label:after {
+  content: '';
+  position: absolute;
+  top: 5px;
+  left: 5px;
+  width: 15px;
+  height: 15px;
+  border: 2px solid $dolor;
+  border-radius: 90px;
+  transition: 0.3s;
+}
+
+input:checked + label {
+  background: transparent;
+}
+
+input:checked + label:after {
+  left: calc(100% - 5px);
+  transform: translateX(-100%);
+}
+
+label:active:after {
+  width: 15px;
+}
+.cambio-vista {
+  height: 150px;
+  justify-content: space-around;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
 }
 
 .archivo {
@@ -278,5 +427,9 @@ li {
     top: 20px;
     position: relative;
   }
+}
+
+.Group_581 {
+  display: flex;
 }
 </style>
