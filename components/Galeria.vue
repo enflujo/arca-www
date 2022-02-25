f910ebc2be054ff29659560e59d1fc631a265d63
Juan Camilo González <j.gonzalezj@uniandes.edu.co>
2022-02-25 17:36:52 +0000

    🚧 En proceso, faltan imagenes para terminar

--- components/Galeria.vue
+++ components/Galeria.vue
@@ -1,33 +1,23 @@
 <template>
-  <span>
-    <div class="todas-images">
-      <div v-for="(obra, i) in obras" :key="`obra-${i}`" class="imagen">
-        <nuxt-link :to="`/imagen/${obra.arca_id}`"
-          ><img :src="urlImagen(obras[i].imagen)" :alt="obras[i].titulo" />
-          <div class="informacion-hover">
-            <h2 v-if="obra.titulo.length > 40" class="nombre-obra-hover">{{ obra.titulo.substr(0, 30) + '\u2026' }}</h2>
-            <h2 v-else class="nombre-obra-hover">{{ obra.titulo }}</h2>
-            <h3 class="nombre-autor-hover">{{ obra.autor.nombre }} {{ obra.autor.apellido }}</h3>
-            <div class="pais-hover">
-              <p v-if="obra.ubicacion_actual.nombre != null">Ubicación actual: {{ obra.ubicacion_actual.nombre }}</p>
-              <!-- <div v-if="obra.origin_country_id != null && obra.actual_country_id != null">
-                <p v-if="obra.origin_country_id.id != obra.actual_country_id.id">
-                  País de origen: {{ obra.origin_country_id.name_spanish }}
-                </p>
-              </div> -->
-              <!-- <div v-if="obra.origin_country_id != null && obra.actual_country_id == null">
-                País de origen: {{ obra.origin_country_id.name_spanish }}
-              </div> -->
-              <span class="fecha-hover">
-                {{ obra.fechas_actividad }}
-              </span>
-            </div>
-            <!-- <h3 class="boton-detalles">DETALLES -></h3> -->
+  <div class="galeria">
+    <div v-for="(obra, i) in obras" :key="`obra-${i}`" class="imagen">
+      <nuxt-link :to="`/imagen/${obra.arca_id}`">
+        <img :src="urlImagen(obras[i].imagen)" :alt="obras[i].titulo" />
+
+        <div class="informacion-hover">
+          <h2 v-if="obra.titulo.length > 40" class="nombre-obra-hover">{{ obra.titulo.substr(0, 30) + '\u2026' }}</h2>
+          <h2 v-else class="nombre-obra-hover">{{ obra.titulo }}</h2>
+          <h3 class="nombre-autor-hover">{{ obra.autor.nombre }} {{ obra.autor.apellido }}</h3>
+          <div class="pais-hover">
+            <p v-if="obra.ubicacion_actual.nombre != null">Ubicación actual: {{ obra.ubicacion_actual.nombre }}</p>
+            <span class="fecha-hover">
+              {{ obra.fechas_actividad }}
+            </span>
           </div>
-        </nuxt-link>
-      </div>
+        </div>
+      </nuxt-link>
     </div>
-  </span>
+  </div>
 </template>
 
 <script>
@@ -48,139 +38,83 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.imagenes {
-  width: 80vw;
-  img {
-    // width: 160%;
-    // height: auto;
-  }
-  .caja-inicial {
-    display: flex;
-    justify-content: space-between;
-    .caja-descripcion {
-      height: auto;
-      display: flex;
-      flex-direction: row;
-      text-align: initial;
-      overflow: scroll;
-      .descripcion {
-        width: 400px;
-        background-color: #af2828;
-        padding: 2em;
-        justify-content: space-around;
-        display: flex;
-        flex-direction: column;
-        .titulo-obra {
-          color: $claridad;
-          font-weight: 300;
-        }
-        .subtitulo-obra {
-          color: $claridad;
-          font-weight: 500;
-          text-transform: uppercase;
-          letter-spacing: 5px;
-        }
-        .descripcion-obra {
-          color: $claridad;
-          line-height: 1.5;
-          letter-spacing: -0.4px;
-          font-size: 12px;
-        }
-        .boton-ver {
-          border: 1px solid $claridad;
-          padding: 1em;
-          border-radius: 25px;
-          color: $claridad;
-          letter-spacing: 6px;
-          letter-spacing: 6px;
-          font-size: 11px;
-          align-self: center;
-        }
-      }
-    }
-  }
-  .caja-secundaria {
-    display: flex;
-    justify-content: space-between;
-    top: 20px;
-    position: relative;
-  }
-}
-.todas-images {
-  display: grid;
-  width: 100%;
-  position: absolute;
-  height: calc(100vh - 140px);
-  grid-gap: 10px;
-  padding: 10px;
-  grid-template-columns: repeat(auto-fill, minmax(20%, 1fr));
-  grid-auto-rows: minmax(32.8%, auto);
-  grid-auto-flow: dense;
-  overflow-y: scroll;
+.galeria {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  // display: grid;
+  // height: calc(100vh - 140px);
+  // grid-gap: 10px;
+  // padding: 10px;
+  // grid-template-columns: repeat(auto-fill, minmax(20%, 1fr));
+  // grid-auto-rows: minmax(32.8%, auto);
+  // grid-auto-flow: dense;
+
   .imagen {
-    border-radius: 10px;
-    overflow: hidden;
-    a {
-      position: relative;
-      display: block;
-      width: 100%;
-      min-height: 100%;
-    }
+    // border-radius: 10px;
+    // overflow: hidden;
+    // a {
+    //   position: relative;
+    //   display: block;
+    //   width: 100%;
+    //   min-height: 100%;
+    // }
   }
+
   .informacion-hover {
-    position: absolute;
-    color: $claridad !important;
-    padding: 20px;
-    opacity: 0;
-    background-color: $dolor;
-    width: 100%;
-    height: 100%;
-    left: 0;
-    top: 0;
-    text-transform: none;
-    z-index: 9;
-    .nombre-obra-hover {
-      color: $claridad;
-      letter-spacing: 0;
-      font-size: 1.4em;
-      font-family: $fuentePrincipal;
-    }
-    .nombre-autor-hover {
-      color: $claridad;
-      letter-spacing: 0;
-      font-size: 1em;
-      font-weight: 350;
-      position: relative;
-      top: 8px;
-    }
-    .pais-hover {
-      letter-spacing: 0;
-      margin-top: 10px;
-      line-height: 1.6;
-      font-size: 0.8em;
-      font-weight: 100;
-      position: relative;
-      top: 0px;
-    }
-    .fecha-hover {
-      letter-spacing: 0;
-      margin-top: 10px;
-      line-height: 1.7;
-      font-weight: 100;
-      position: relative;
-      top: 0px;
-    }
+    // position: absolute;
+    // color: $claridad !important;
+    // padding: 20px;
+    // opacity: 0;
+    // background-color: $dolor;
+    // width: 100%;
+    // height: 100%;
+    // left: 0;
+    // top: 0;
+    // text-transform: none;
+    // z-index: 9;
+    // .nombre-obra-hover {
+    //   color: $claridad;
+    //   letter-spacing: 0;
+    //   font-size: 1.4em;
+    //   font-family: $fuentePrincipal;
+    // }
+    // .nombre-autor-hover {
+    //   color: $claridad;
+    //   letter-spacing: 0;
+    //   font-size: 1em;
+    //   font-weight: 350;
+    //   position: relative;
+    //   top: 8px;
+    // }
+    // .pais-hover {
+    //   letter-spacing: 0;
+    //   margin-top: 10px;
+    //   line-height: 1.6;
+    //   font-size: 0.8em;
+    //   font-weight: 100;
+    //   position: relative;
+    //   top: 0px;
+    // }
+    // .fecha-hover {
+    //   letter-spacing: 0;
+    //   margin-top: 10px;
+    //   line-height: 1.7;
+    //   font-weight: 100;
+    //   position: relative;
+    //   top: 0px;
+    // }
   }
   .informacion-hover:hover {
-    opacity: 0.95;
-    transition: 0.5s;
+    // opacity: 0.95;
+    // transition: 0.5s;
   }
   .boton-detalles {
-    font-weight: 500;
-    font-size: 0.75em;
-    position: relative;
-    top: 20px;
-    text-align: flex-end;
+    // font-weight: 500;
+    // font-size: 0.75em;
+    // position: relative;
+    // top: 20px;
+    // text-align: flex-end;
   }
 }
 </style>
