123d15ef8ecb6baf5da899ce62ea16ee9fa37720
Juan Camilo González <j.gonzalezj@uniandes.edu.co>
2023-01-10 19:22:07 -0500

    🗑️ Limpiar

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,4 @@
 import { defineNuxtConfig } from 'nuxt/config';
-import { apiBase } from './config/general';
-
-const isDev = process.env.NODE_ENV === 'development';
-
-const server = {
-  host: isDev ? 'localhost' : '0.0.0.0',
-  port: isDev ? 3000 : 4043,
-};
 
 export default defineNuxtConfig({
   modules: ['@pinia/nuxt'],
