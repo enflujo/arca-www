365efb3b03447fa7a19725d3b71cb12aaf827ddd
anattolia <anattolia@gmail.com>
2023-02-03 16:01:43 -0500

    🚧 Paginación (falta en autor)

--- utilidades/queries.js
+++ utilidades/queries.js
@@ -16,6 +16,9 @@ export const nombrePorSlug = (coleccion, slug) => {
     ${coleccion}(filter: { slug: { _eq: "${slug}" } }, limit: 1) {
       id
       nombre
+      obras_func {
+        count
+      }
     }
   }
   `;
@@ -29,11 +32,11 @@ export const nombrePorSlug = (coleccion, slug) => {
  * @param {boolean} m2m  ¿Es de una relación M2M?
  * @returns
  */
-export const obrasPorSlug = (coleccion, slug, m2m = false) => {
+export const obrasPorSlug = (coleccion, slug, m2m = false, pagina = 1) => {
   return gql`
   query {
     ${coleccion}(filter: { slug: { _eq: "${slug}" } }, limit: 1) {
-      obras(limit: ${cerebroArchivo.obrasPorPagina}) {
+      obras(limit: ${cerebroArchivo.obrasPorPagina}, page: ${pagina}) {
         ${m2m ? 'obras_id {' : ''}
         id
         registro
