ed45f0336cbdc544675c3dd8695fe2ff8a550cf1
anattolia <anattolia@gmail.com>
2023-02-24 18:31:32 -0500

    💩 Grilla sin redondear centenas

--- components/Grafica/Colombinas.vue
+++ components/Grafica/Colombinas.vue
@@ -6,6 +6,8 @@ const contenedor = ref(null);
 const anchoPantalla = ref(0);
 const datos = ref([]);
 const maximoObras = ref(0);
+const divisionesGrilla = [...Array(10).keys()];
+//const longitudGrilla = ref(0);
 
 const props = defineProps({
   datos: Array,
@@ -29,40 +31,67 @@ onMounted(() => {
   maximoObras.value = datosOrdenados[0].obras_func.count;
   datos.value = datosOrdenados;
   anchoPantalla.value = contenedor.value.clientWidth;
+
+  // let calcularGrilla = function () {
+  //   let l = maximoObras.value < 10000 ? 100 : 200;
+  //   console.log(maximoObras.value);
+  //   return l;
+  // };
+
+  // longitudGrilla.value = calcularGrilla();
 });
 </script>
 
 <template>
-  <ul ref="contenedor">
-    <li v-for="elemento in datos" :key="elemento.slug">
-      <NuxtLink class="nombre fila" :to="`/archivo/${props.coleccion}/${elemento.slug}`">{{
-        elemento.nombre
-      }}</NuxtLink>
+  <div id="contenedorGrafica">
+    <div id="grilla" :style="`width:${convertirEscala(maximoObras, 1, maximoObras, 0, anchoPantalla / 1.5)}px`">
+      <span
+        class="divisionGrilla"
+        v-for="i in divisionesGrilla"
+        :key="`división ${i}`"
+        :style="`width:${convertirEscala(maximoObras, 1, maximoObras, 0, anchoPantalla / 1.5) / 10}px`"
+      >
+        <p class="valorGrilla">
+          {{ Math.ceil((maximoObras / 10) * i) }}
+        </p>
+      </span>
+    </div>
+    <ul ref="contenedor">
+      <li v-for="elemento in datos" :key="elemento.slug">
+        <NuxtLink class="nombre fila" :to="`/archivo/${props.coleccion}/${elemento.slug}`">{{
+          elemento.nombre
+        }}</NuxtLink>
 
-      <NuxtLink class="elementoColombina fila" :to="`/archivo/${props.coleccion}/${elemento.slug}`">
-        <div class="colombina">
-          <span
-            class="lineaColombina"
-            :style="`width:${convertirEscala(
-              elemento.obras_func.count,
-              1,
-              maximoObras,
-              0,
-              anchoPantalla / 1.5
-            )}px; background-color:${buscarColor(elemento.obras_func.count)}`"
-          ></span>
-          <span class="circuloColombina" :style="`background-color:${buscarColor(elemento.obras_func.count)}`"></span>
-          <span class="conteoObras">{{ elemento.obras_func.count }}</span>
-        </div>
-      </NuxtLink>
-    </li>
-  </ul>
+        <NuxtLink class="elementoColombina fila" :to="`/archivo/${props.coleccion}/${elemento.slug}`">
+          <div class="colombina">
+            <span
+              class="lineaColombina"
+              :style="`width:${convertirEscala(
+                elemento.obras_func.count,
+                1,
+                maximoObras,
+                0,
+                anchoPantalla / 1.5
+              )}px; background-color:${buscarColor(elemento.obras_func.count)}`"
+            ></span>
+            <span class="circuloColombina" :style="`background-color:${buscarColor(elemento.obras_func.count)}`"></span>
+            <span class="conteoObras">{{ elemento.obras_func.count }}</span>
+          </div>
+        </NuxtLink>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <style lang="scss" scoped>
+#contenedorGrafica {
+  position: relative;
+}
 ul {
   display: table;
   width: 100%;
+  position: relative;
+  margin-top: 2em;
 
   li {
     display: table-row;
@@ -78,6 +107,7 @@ ul {
     padding-right: 0.5em;
     font-family: var(--fuenteMenu);
     font-weight: var(--fuenteMenuPeso);
+    width: 200px;
   }
 }
 .nombre {
@@ -87,11 +117,8 @@ ul {
   &:hover {
     font-weight: bold;
   }
-
-  &:hover::before {
-    content: '+ ';
-  }
 }
+
 .colombina {
   display: flex;
   align-items: center;
@@ -102,8 +129,8 @@ ul {
   }
   .circuloColombina {
     display: block;
-    height: 7px;
-    width: 7px;
+    height: 8px;
+    width: 8px;
     border-radius: 50%;
   }
 
@@ -111,6 +138,28 @@ ul {
     color: #788989;
     font-size: 0.75em;
     padding-left: 0.4em;
+  }
+}
+
+#grilla {
+  border-top: #78898955 dashed 1px;
+  display: flex;
+  height: 100%;
+  position: absolute;
+  left: 204px;
+  font-size: 0.7em;
+  overflow: visible;
+  margin-top: 2em;
+  color: #78898955;
+
+  .divisionGrilla {
+    display: block;
+    border-right: #78898955 solid 1px;
+  }
+
+  .valorGrilla {
+    position: absolute;
+    top: -2em;
   }
 }
 </style>
