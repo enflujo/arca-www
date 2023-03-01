286d91541bd21a6584380a7618eac3081553d396
Daniel Hoyos Q <enflujo@uniandes.edu.co>
2023-03-01 10:24:06 -0500

    ✨ Complemento Torta

---
+++ components/Grafica/Torta.vue
@@ -0,0 +1,17 @@
+<script setup>
+const props = defineProps({
+  datos: Array,
+});
+
+function totalObras(datos) {
+  const total = Math(datos);
+}
+
+function convertirPorcentaje(datos) {
+  const total = totalObras(datos);
+}
+</script>
+
+<template></template>
+
+<style></style>
