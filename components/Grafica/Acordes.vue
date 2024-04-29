bcbfe9173932b82cc8f4821dd4d6e473c485dda7
Juan Camilo González <j.gonzalezj@uniandes.edu.co>
2024-04-29 18:38:21 -0500

    🚧

---
+++ components/Grafica/Acordes.vue
@@ -0,0 +1,17 @@
+<script setup>
+interface Props {
+  ancho: number;
+  alto?: number;
+  radio?: number;
+  color: string;
+  total: number;
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+  <svg></svg>
+</template>
+
+<style lang="scss"></style>
